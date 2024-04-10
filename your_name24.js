93c86b3e19d46e3a08f06fd6d24764e9f94b5332
mikeoneill27 <166660133+mikeoneill27@users.noreply.github.com>
2024-04-10 17:23:25 -0400

    Michael_ONeill24.js

--- your_name24.js
+++ your_name24.js
@@ -1,5 +1,5 @@
 // Initialize Leaflet map
-var map = L.map('nymap').setView([40.681801312931455, -73.9571896025164], 13); // Set center to New York City and zoom level
+var map = L.map('nymap').setView([40.56068599677129, -74.14733985093602], 13); // Set center to New York City and zoom level
 
 // Add a tile layer to the map (for example, OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
