5928edd07f142a8629244fc39458eda17d6600f2
mikeoneill27 <166660133+mikeoneill27@users.noreply.github.com>
2024-04-10 17:24:32 -0400

    Michael_ONeill24.js

--- your_name24.js
+++ your_name24.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add a marker to the map for New York City
-var marker = L.marker([40.68965691085725, -73.96537769031497]).addTo(map);
+var marker = L.marker([40.56068599677129, -74.14733985093602]).addTo(map);
 
 // Add a popup to the marker
-marker.bindPopup("<b>Luigi's Pizza</b><br>This is a Leaflet map.").openPopup();
+marker.bindPopup(Great Kills Little League).openPopup();
